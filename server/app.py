88cfcd5891b1b7af5dcb73701ecb4bd1eec92931
Youngpuffy <levimuturi2@gmail.com>
2025-06-11 13:47:19 +0300

    complete assignment

--- server/app.py
+++ server/app.py
@@ -3,6 +3,40 @@
 from flask import Flask
 
 app = Flask(__name__)
+@app.route('/')
+def index():
+    return '<h1>Python Operations with Flask Routing and Views</h1>'
+
+@app.route('/print/<string:parameter>')
+def print_string(parameter):
+    print(parameter)
+    return parameter
+
+@app.route('/count/<int:parameter>')
+def count(parameter):
+    numbers = '\n'.join(str(i) for i in range(parameter)) + '\n'
+    return numbers
+
+@app.route('/math/<int:num1>/<operation>/<int:num2>')
+def math(num1, operation, num2):
+    if operation == '+':
+        result = num1 + num2
+    elif operation == '-':
+        result = num1 - num2
+    elif operation == '*':
+        result = num1 * num2
+    elif operation == 'div':
+        if num2 == 0:
+            return "Error: Division by zero"
+        result = num1 / num2
+    elif operation == '%':
+        if num2 == 0:
+            return "Error: Modulo by zero"
+        result = num1 % num2
+    else:
+        return "Error: Unsupported operation"
+    
+    return str(result)
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
